ab8b019452f75b67b1b0a9e2aed6dfa178d7d9e1
Chance Mims <chancemims1@gmail.com>
2019-12-07 23:15:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = []
+
+  pigeon_data.reduce do |memo, (key, value)|
+    value.reduce do |memo, (key2, value2)|
+      names << value2
+    end
+  end
+  names.flatten.uniq
+
+  names.each({}) do |memo|
+    memo[:names] = names
+    memo
+  end
+
 end
